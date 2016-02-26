6fb93d2eec5f1997e16a3742f8066a0509a904bd
tizot <masset.camille@gmail.com>
2016-02-26 15:21:20 +0100

    Data preprocessing finished

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -2,6 +2,7 @@
 import csv
 import datetime
 import pandas as pd
+import numpy as np
 
 # Useful map between columns and numbers in the csv
 #DAY_DS : 2
@@ -38,6 +39,7 @@ string_features = {
 numeric_features = ['CSPL_', ]
 data = [[] for i in range(87)]
 labels = []
+CHUNK_SIZE = 10000
 
 print "Heure de debut : "+datetime.datetime.now().strftime("%I:%M")
 
@@ -54,15 +56,16 @@ for l in labels:
     else:
         expansed_labels.append(l)
 
-with open('train.csv', 'w') as f:
+with open('train_tmp.csv', 'w') as f:
     f.write(";".join(expansed_labels))
+    f.write("\n")
 
     # Parcourir le fichier train_2011_2012.csv et recopier les données dans le nouveau fichier
     with open('train_2011_2012.csv') as csvfile:
         reader = csv.reader(csvfile, delimiter=';', quotechar='"')
 
         for idx, row in enumerate(reader, 1):
-            if idx % 100000 == 0:
+            if idx % CHUNK_SIZE == 0:
                 print("Ligne %d" % idx)
             r = []
             for i, l in enumerate(row):
@@ -72,11 +75,31 @@ with open('train.csv', 'w') as f:
                 else:
                     r.append(l)
             f.write(";".join(r))
+            f.write("\n")
 
 # Nettoyage des données : remplacer les NULL par la moyenne de la colonne (pour les données numériques)
-with open('train.csv', 'w') as f:
-    data = pd.read_csv(f, sep=";", quotechar='"')
-    means = data.mean(numeric_only=True)
-    data.fillna(value=means)
+with open('train_tmp.csv') as f:
+    # Calcul des moyennes des features numériques
+    reader = pd.read_csv(f, sep=";", quotechar='"', iterator=True, chunksize=CHUNK_SIZE)
+    means = pd.Series(np.zeros(reader.get_chunk(1).shape[1]))
+    lines = 0
+    for chunk in reader:
+        old_lines = lines
+        lines += chunk.shape[0]
+        print("Moyenne : bloc l. %d - %d" % (old_lines, lines))
+        means += chunk.mean(numeric_only=True)
+
+    means = means / lines
+
+with open('train_tmp.csv') as f:
+    # Affectation des moyennes aux valeurs NaN
+    reader = pd.read_csv(f, sep=";", quotechar='"', iterator=True, chunksize=CHUNK_SIZE)
+    idx = 0
+    for c in reader:
+        print("Ecriture : bloc no. %d" % (idx))
+        idx += 1
+        c.fillna(value=means)
+        c.to_csv('train_data.csv', sep=';', header=(idx==0), mode='a') # write header only for first chunk, and append results
+
 
 print "Heure de fin : "+datetime.datetime.now().strftime("%I:%M")
